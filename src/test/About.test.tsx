a2cb336605e300a2d907690dd08f1a185ed41ac7
Zura Shubitidze <157276781+ZuShubitidze@users.noreply.github.com>
2025-09-23 17:57:50 +0400

    changed test files

--- src/test/About.test.tsx
+++ src/test/About.test.tsx
@@ -1,18 +1,20 @@
 import About from "../pages/Aboutpage";
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 
-test("renders About Page correctly", () => {
-  render(<About />);
-  expect(screen.getByText(/kaxa/i)).toBeInTheDocument();
-});
+describe("Aboutpage component", () => {
+  test("renders About Page correctly", () => {
+    render(<About />);
+    expect(screen.getByText(/kaxa/i)).toBeInTheDocument();
+  });
 
-test("renders About Page Header correctly", () => {
-  render(<About />);
-  expect(screen.getByText(/გვერდი ჩემს შესახებ/i)).toBeInTheDocument();
-});
+  test("renders About Page Header correctly", () => {
+    render(<About />);
+    expect(screen.getByText(/გვერდი ჩემს შესახებ/i)).toBeInTheDocument();
+  });
 
-test("renders About Page Phone Number correctly", () => {
-  render(<About />);
-  expect(screen.getByText(/599 92 35 98/i)).toBeInTheDocument();
+  test("renders About Page Phone Number correctly", () => {
+    render(<About />);
+    expect(screen.getByText(/599 92 35 98/i)).toBeInTheDocument();
+  });
 });
--- src/test/Contact.test.tsx
+++ src/test/Contact.test.tsx
@@ -1,21 +1,23 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import Contact from "../pages/Contactpage";
 import { screen } from "@testing-library/react";
 import { render } from "@testing-library/react";
 
-test("renders Contact Page Header correctly", () => {
-  render(<Contact />);
-  expect(screen.getByText(/საკონტაქტო/i)).toBeInTheDocument();
-});
+describe("Contactpage component", () => {
+  test("renders Contact Page Header correctly", () => {
+    render(<Contact />);
+    expect(screen.getByText(/საკონტაქტო/i)).toBeInTheDocument();
+  });
 
-test("renders Contact Page Email correctly", () => {
-  render(<Contact />);
-  expect(
-    screen.getByText(/kaxabershubitidze123@gmail.com/i)
-  ).toBeInTheDocument();
-});
+  test("renders Contact Page Email correctly", () => {
+    render(<Contact />);
+    expect(
+      screen.getByText(/kaxabershubitidze123@gmail.com/i)
+    ).toBeInTheDocument();
+  });
 
-test("renders Contact Page Phone Number correctly", () => {
-  render(<Contact />);
-  expect(screen.getByText(/599 92 35 98/i)).toBeInTheDocument();
+  test("renders Contact Page Phone Number correctly", () => {
+    render(<Contact />);
+    expect(screen.getByText(/599 92 35 98/i)).toBeInTheDocument();
+  });
 });
