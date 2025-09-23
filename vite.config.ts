b6c77e6b71246c0485ebf994c7ca26d35aafd483
Zura Shubitidze <157276781+ZuShubitidze@users.noreply.github.com>
2025-09-23 18:02:22 +0400

    fixed viteConfig Setupfiles location2

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
